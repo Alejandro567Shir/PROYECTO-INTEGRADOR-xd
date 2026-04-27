6129796f582121545cccafb5fa9286d1e721de6f
Alejandro567Shir <138833827+Alejandro567Shir@users.noreply.github.com>
2026-04-27 12:18:37 -0500

    Sigan viendoxdxd

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,14 +3,11 @@ const pool = require("../config/db");
 const login = async (req, res) => {
   try {
     const { correo, clave, rol } = req.body;
-
-    // 1. Buscamos al usuario y traemos su pase VIP (es_comision)
     const result = await pool.query(
       "SELECT id, nombre, correo, rol, es_comision FROM usuarios WHERE correo = $1 AND clave = $2",
       [correo, clave]
     );
 
-    // Si la clave o el correo están mal
     if (result.rows.length === 0) {
       return res.json({
         success: false,
@@ -20,7 +17,6 @@ const login = async (req, res) => {
 
     const usuario = result.rows[0];
 
-    // 2. Si entra a su panel normal (Ej. Roberto entrando a Director)
     if (usuario.rol === rol) {
       return res.json({
         success: true,
@@ -28,16 +24,15 @@ const login = async (req, res) => {
       });
     }
 
-    // 3. LA MAGIA: Si quiere entrar a Comisión y tiene Pase VIP
+  
     if (rol === "COMISION DE TITULACION" && usuario.es_comision === true) {
-      usuario.rol = "COMISION DE TITULACION"; // Engañamos a React temporalmente
+      usuario.rol = "COMISION DE TITULACION"; 
       return res.json({
         success: true,
         usuario: usuario,
       });
     }
 
-    // 4. Si falla todo lo anterior
     return res.json({
       success: false,
       mensaje: "No tienes permiso para ingresar a este panel",
